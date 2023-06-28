5b36ce75009cdcabcdd891e1bad2a5985f4bb4b5
mr_ozin <mrozin.dev@gmail.com>
2023-06-28 19:17:23 +0900

    fix test

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,10 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Mr_ozin' })).toBeVisible();
+});
+
+test('project page has expected h1', async ({ page }) => {
+	await page.goto('/project');
+	await expect(page.getByRole('heading', { name: 'Project' })).toBeVisible();
 });
