b822318aab166c6fe25ea53deeb7285fd9a471bd
mr_ozin <mrozin.dev@gmail.com>
2023-07-02 13:56:50 +0900

    fix components path and add Hatena logo

--- src/lib/components/Links.svelte
+++ src/lib/components/Links.svelte
@@ -3,19 +3,28 @@
 	import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 	import ZennLogo from './ZennLogo.svelte';
 	import type { Link } from '$lib/types';
+	import HatenaLogo from './HatenaLogo.svelte';
 
 	export let links: Link[];
 </script>
 
 <div class="links">
 	{#each links as link}
-		<a href={link.href} class="link-item" target="_blank" rel="noopener noreferrer">
+		<a
+			href={link.href}
+			class="link-item"
+			title={link.name}
+			target="_blank"
+			rel="noopener noreferrer"
+		>
 			{#if link.name === 'GitHub'}
 				<Fa icon={faGithub} />
 			{:else if link.name === 'Twitter'}
 				<Fa icon={faTwitter} />
 			{:else if link.name === 'Zenn'}
 				<ZennLogo width={'2rem'} height={'2rem'} />
+			{:else if link.name === 'Hatena'}
+				<HatenaLogo width={'2rem'} height={'2rem'} />
 			{:else}
 				<p>{link.name}</p>
 			{/if}
