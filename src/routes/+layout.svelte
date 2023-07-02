cd78f641c28c457dc18219b73ef01ddea1153c23
mr_ozin <mrozin.dev@gmail.com>
2023-07-02 13:27:29 +0900

    move meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import './global.css';
 </script>
 
+<svelte:head>
+	<title>Mr_ozin</title>
+	<meta name="description" content="Profile site about Mr_ozin" />
+	<meta property="og:title" content="Mr_ozin" />
+	<meta property="og:site_title" content="Mr_ozin" />
+	<meta property="og:description" content="Profile site about Mr_ozin" />
+	<meta property="og:url" content="https://mrozin.me/" />
+	<meta property="og:image" content="https://mrozin.me/images/author.jpg" />
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 <div class="site-container">
 	<slot />
 </div>
