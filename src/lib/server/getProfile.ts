278a9bf40fa651469c909897dab42b6201359e36
mr_ozin <mrozin.dev@gmail.com>
2023-10-27 19:50:29 +0900

    fix headless api request

--- src/lib/server/getProfile.ts
+++ src/lib/server/getProfile.ts
@@ -1,10 +1,13 @@
 import { CMS_API_KEY, CMS_ENDPOINT } from '$env/static/private';
 import type { ProfileResponse } from '$lib/types';
 
+const baseURL = CMS_ENDPOINT ?? '';
+const apiKey = CMS_API_KEY ?? '';
+
 export const fetchProfile = async (): Promise<ProfileResponse> => {
-	const res = await fetch(`${CMS_ENDPOINT}/profile`, {
+	const res = await fetch(`${baseURL}/profile`, {
 		headers: {
-			'X-MICROCMS-API-KEY': CMS_API_KEY
+			'X-MICROCMS-API-KEY': apiKey
 		}
 	});
 
