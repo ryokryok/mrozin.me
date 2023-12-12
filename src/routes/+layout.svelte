2e23f39f6f685d27e72522e1623247835eaec146
mr_ozin <mrozin.dev@gmail.com>
2023-12-12 22:24:08 +0900

    write e2e tests, for OGP, lang, heading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,13 @@
   <title>Mr_ozin</title>
   <meta name="description" content="Profile site about Mr_ozin" />
   <meta property="og:title" content="Mr_ozin" />
-  <meta property="og:site_title" content="Mr_ozin" />
   <meta property="og:description" content="Profile site about Mr_ozin" />
   <meta property="og:url" content="https://mrozin.me/" />
   <meta property="og:image" content="https://mrozin.me/images/author.jpg" />
   <meta property="og:type" content="website" />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@Mr_ozin" />
+  <meta name="twitter:creator" content="@Mr_ozin" />
 </svelte:head>
 
 <slot />
